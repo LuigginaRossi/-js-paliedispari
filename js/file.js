67653036d610de4e832ae7727b8d36f9f8470d9e
LuigginaRossi <luigginavaldivia@gmail.com>
2022-11-08 18:24:05 +0100

    aggiunta funzione di verifica, es:palidroma

--- js/file.js
+++ js/file.js
@@ -4,9 +4,34 @@ console.log(wordUser);
 
 // creo array della parola inserita dall'user
 const arrayWordUser = wordUser.split('');
-console.log(arrayWordUser);
-
+console.log("array:"+ arrayWordUser);
 
 // inverti ordine
 arrayWordUser.reverse();
-console.log(arrayWordUser);
+console.log("inverto array:"+ arrayWordUser);
+
+
+//inverto di nuovo ordine array:
+//const arrayUserReverse = arrayWordUser.reverse();
+//console.log("inverto di nuovo array:"+arrayUserReverse);
+
+
+/**
+ * Controlla che la parola inserita dall'utente sia una parola leggibile anche al contrario
+ * @param {String} word  
+ */
+function checkWordPalidr( word ){
+    for (let i = 0; i < arrayWordUser.length; i++) {
+// let i = 0;
+// i++;
+        if( wordUser[i] === arrayWordUser[i]){
+            //return "Ottimo lavoro! Hai ins[rito una parola palidroma!";
+            console.log("Ottimo lavoro! Hai inserito una parola palidroma!")
+        }else{
+            alert("Attenzione! La parola non è Palidroma! Ricarica la pagina  prova ancora!")
+        }
+   }
+    
+}
+
+checkWordPalidr();
