6067c2322f768c17b171d9ce966db4ab7b1acbfd
LuigginaRossi <luigginavaldivia@gmail.com>
2022-11-08 18:47:03 +0100

    modifica funzione

---
+++ js/PariDispari.js
@@ -0,0 +1,11 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+// Se l’utente ha scritto “pari” e la somma è pari, ha vinto l’utente
+// Se l’utente ha scritto “pari” e la somma è dispari, ha vinto il pc
+
+
+const userPrediction = prompt("Inserisci 'pari' se credi che la somma tra il tuo numero e il numero di Ai sarà pari altrimenti inserisci 'dispari'! Buona fortuna! ")
--- js/palidroma.js
+++ js/palidroma.js
@@ -11,28 +11,23 @@ arrayWordUser.reverse();
 console.log("inverto array:"+ arrayWordUser);
 
 
-//inverto di nuovo ordine array:
-//const arrayUserReverse = arrayWordUser.reverse();
-//console.log("inverto di nuovo array:"+arrayUserReverse);
-
-
 /**
  * Controlla che la parola inserita dall'utente sia una parola leggibile anche al contrario
  * @param {String} word  
  */
 function checkWordPalidr( word ){
-    for (let i = 0; i < arrayWordUser.length; i++) {
-// let i = 0;
-// i++;
+//   for (let i = 0; i < arrayWordUser.length; i++) {
+ let i = 0;
+ i++;
         if( wordUser[i] === arrayWordUser[i]){
             //return "Ottimo lavoro! Hai ins[rito una parola palidroma!";
             console.log("Ottimo lavoro! Hai inserito una parola palidroma!")
         }else{
-            alert("Attenzione! La parola non è Palidroma! Ricarica la pagina  prova ancora!")
+            alert("Attenzione! La parola inserita non è Palidroma! Ricarica la pagina prova ancora!")
         }
    }
     
-}
+//}
 
 checkWordPalidr();
 
